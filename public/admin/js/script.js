59290fbb48e297fbeca9004df574dd3d129f6971
Nhut Nam <lenguyennhutnam91@gmail.com>
2024-06-16 02:33:20 +0700

    add update multi status - di ngu thoi

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -51,7 +51,7 @@ pageBtn.forEach((page) => {
 const changeStatusBtns = document.querySelectorAll("[change-status-btn]");
 changeStatusBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        const productId = btn.getAttribute("product-id");
+        const productId = btn.closest("tr").getAttribute("product-id");
         const API = `/admin/products/change-status/${productId}`;
         fetch(API, {
             method: "PATCH",
@@ -67,3 +67,42 @@ changeStatusBtns.forEach((btn) => {
             });
     });
 });
+// End change status
+
+// Change multi status
+const activeAllBtn = document.querySelector("input[name='activeAll']");
+const isActiveBtns = document.querySelectorAll("input[name='isActive']");
+const updateMultiStatusBtn = document.querySelector(
+    "button[update-multi-status-btn]"
+);
+// active all event
+activeAllBtn.addEventListener("change", (e) => {
+    isActiveBtns.forEach((btn) => {
+        btn.checked = e.target.checked;
+    });
+});
+// check active event
+isActiveBtns.forEach((btn) => {
+    btn.addEventListener("change", (e) => {
+        const isActiveBtnChecked = document.querySelectorAll(
+            "input[name='isActive']:checked"
+        );
+        if (isActiveBtnChecked.length == isActiveBtns.length) {
+            activeAllBtn.checked = true;
+        } else {
+            activeAllBtn.checked = false;
+        }
+    });
+});
+// update multi status btn
+updateMultiStatusBtn.addEventListener("click", (e) => {
+    const isActiveBtnChecked = document.querySelectorAll(
+        "input[name='isActive']:checked"
+    );
+    const checkedId = Array.from(isActiveBtnChecked).map((btn) => {
+        console.log(btn.closest("tr").getAttribute("product-id"));
+        return btn.closest("tr").getAttribute("product-id");
+    });
+    console.log(checkedId);
+});
+// End change multi status
